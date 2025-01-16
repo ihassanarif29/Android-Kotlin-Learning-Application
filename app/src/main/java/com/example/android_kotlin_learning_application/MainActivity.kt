76c8154a8bfbfd7916886129340dc74f2cce2ce9
ihassanarif29 <ihassanarif29@gmail.com>
2025-01-17 01:47:05 +0500

    Toast Function

--- app/src/main/java/com/example/android_kotlin_learning_application/MainActivity.kt
+++ app/src/main/java/com/example/android_kotlin_learning_application/MainActivity.kt
@@ -1,6 +1,8 @@
  package com.example.android_kotlin_learning_application
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,13 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+
+        val showToastButton = findViewById<Button>(R.id.buttonToast)
+
+        showToastButton.setOnClickListener {
+            Toast.makeText(this,"Hi this is my Toast",Toast.LENGTH_SHORT).show()
         }
     }
 }
